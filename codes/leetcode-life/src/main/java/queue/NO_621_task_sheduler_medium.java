28c5ae93ae20c4311de8797ba6ee5e850a5eb23a
dave <sunrong1990@126.com>
2021-03-22 07:09:32 +0800

    Stack的括号类算法题

---
+++ codes/leetcode-life/src/main/java/queue/NO_621_task_sheduler_medium.java
@@ -0,0 +1,20 @@
+package queue;
+
+/**
+ * 问题类型：
+ * 队列问题
+ * <p>
+ * Description
+ * 在任何一个单位时间，CPU 可以完成一个任务，或者处于待命状态。
+ * 两个 相同种类 的任务之间必须有长度为整数 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
+ * 计算完成所有任务所需要的 最短时间
+ *
+ * @author dave
+ * @since 2021/3/22 7:07
+ */
+public class NO_621_task_sheduler_medium {
+
+    public int leastInterval(char[] tasks, int n) {
+
+    }
+}
